9cd822d6a7fe5948e29ea3febbb2e361482aaaa0
BossBGG <kitti.darat.B@gmail.com>
2025-11-25 17:34:52 +0700

    Implement Stocks

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -31,7 +31,7 @@ const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: Stock
           className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
         >
           <Avatar className="h-8 w-8">
-            <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW89DxjlweTcfLtQ50etebx5jmNYGl6vM2cQ&s" />
+            <AvatarImage src="https://cdn-icons-png.freepik.com/512/8428/8428718.png" />
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
               {user.name[0]}
             </AvatarFallback>
@@ -48,7 +48,7 @@ const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: Stock
         <DropdownMenuLabel>
           <div className="flex relative items-center gap-3 py-2">
             <Avatar className="h-10 w-10">
-              <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW89DxjlweTcfLtQ50etebx5jmNYGl6vM2cQ&s" />
+              <AvatarImage src="https://cdn-icons-png.freepik.com/512/8428/8428718.png" />
               <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                 {user.name[0]}
               </AvatarFallback>
